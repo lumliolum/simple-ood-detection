e2fda768b1a66384075d18ed5b80299de9335d49
lumliolum <mahajan.saisuchith@gmail.com>
2023-04-24 21:31:16 +0000

    updated sh file

--- train.sh
+++ train.sh
@@ -5,23 +5,24 @@ python3 train.py \
     --unknown-classes 3,4 \
     --train-val-test-split 0.8,0.1,0.1 \
     --train-crop-size 32 \
-    --test-resize-size 40 \
+    --test-resize-size 32 \
     --test-crop-size 32 \
     --hflip-prob 0.5 \
     --random-erase-prob 0.1 \
-    --batch-size 8 \
-    --num-workers 2 \
-    --epochs 5 \
-    --model resnet50 \
+    --batch-size 32 \
+    --num-workers 4 \
+    --epochs 30 \
+    --early-stopping-epochs 6 \
+    --model resnet34 \
     --label-smoothing 0.1 \
     --optimizer sgd \
-    --lr 0.5 \
+    --lr 0.001 \
     --lr-min 0 \
     --momentum 0.9 \
     --weight-decay 0.00002 \
     --norm-weight-decay 0.0 \
-    --lr-warmup-epochs 2 \
+    --lr-warmup-epochs 5 \
     --lr-warmup-decay 0.01 \
     --seed 42 \
-    --device cpu \
+    --device cuda \
     --output-dir runs
